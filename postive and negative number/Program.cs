bf066a476db45556d374598beeed46329aadb4ed
RasimAbbasov <rasimra@code.edu.az>
2024-08-20 00:48:35 +0400

    Task added

---
+++ postive and negative number/Program.cs
@@ -0,0 +1,14 @@
+﻿//Verilmish ededin menfi ve ya musbet oldugunu tapan algorithm
+int x = Convert.ToInt32(Console.ReadLine());
+if (x > 0)
+{
+    Console.WriteLine("Eded musbetdir");
+}
+else if (x < 0)
+{
+    Console.WriteLine("Eded menfidir");
+}
+else
+{
+    Console.WriteLine("Eded ne musbet ne menfi ededdir");
+}
