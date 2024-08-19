2b8ef77825db9278616bc74079c1b17728c2cec5
RasimAbbasov <rasimra@code.edu.az>
2024-08-20 00:51:19 +0400

    Task added

---
+++ Verilmish 2 mertebeli ededin en boyuk reqemini tapan algorithm misal/Program.cs
@@ -0,0 +1,14 @@
+﻿//Verilmish 2 mertebeli ededin en boyuk reqemini tapan algorithm misal
+int y, z;
+int x = Convert.ToInt32(Console.ReadLine());
+if (x >= 10 && 100>x)
+{
+    y = x / 10;
+    z = x % 10;
+    if(y>z)  {Console.WriteLine(y);}
+    else if(y<z) {Console.WriteLine(z);}
+    else
+    {Console.WriteLine(y);} 
+}
+else
+{ Console.WriteLine("Eded 2 mertebeli deyil");}
