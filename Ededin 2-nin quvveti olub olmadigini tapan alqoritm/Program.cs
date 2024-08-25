f88bae170303908cd439affaa53407a55feba978
RasimAbbasov <rasimra@code.edu.az>
2024-08-25 13:29:12 +0400

    Task2 added

---
+++ Ededin 2-nin quvveti olub olmadigini tapan alqoritm/Program.cs
@@ -0,0 +1,15 @@
+﻿//Verilmish ededin 2-nin quvveti olub olmadigini tapan alqoritm misal: 8 quvvetidir, 6 quvveti deyil
+int x = Convert.ToInt32(Console.ReadLine());
+if (x <= 0)
+{
+    Console.WriteLine("Melumat duzgun deyil");
+}
+while (x % 2 == 0)
+{
+    x /= 2;
+}
+if (x == 1)
+    Console.WriteLine("Quvvetidir");
+else
+    Console.WriteLine("Quvveti deyil");
+    
