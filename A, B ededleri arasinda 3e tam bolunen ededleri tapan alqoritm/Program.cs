fddfbb17ba437e7091648494fa41ca2aa4eaa906
RasimAbbasov <rasimra@code.edu.az>
2024-08-25 13:28:23 +0400

    Task2 added

---
+++ A, B ededleri arasinda 3e tam bolunen ededleri tapan alqoritm/Program.cs
@@ -0,0 +1,13 @@
+﻿//A, B ededleri arasinda 3e tam bolunen ededleri tapan alqoritm
+int a=Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+while (a <= b)
+{
+    if (a % 3 == 0)
+        Console.WriteLine(a);
+    a++;
+}
+
+
+
+
