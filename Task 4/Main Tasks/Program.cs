f2888a53090270a29e2aa357ecfe56db637e3569
RasimAbbasov <rasimra@code.edu.az>
2024-09-12 16:52:34 +0400

    Task 4 added

---
+++ Task 4/Main Tasks/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace Main_Tasks
+{
+    internal class Program
+    {
+        #region Verilmiş ədədi kvadratına yüksəldən metod
+        //static void Main(string[] args)
+        //{
+        //    int a = Convert.ToInt32(Console.ReadLine());
+        //    Square(a);
+        //}
+        //static void Square(int x)
+        //{
+        //    Console.WriteLine(x * x);
+        //}
+        #endregion
+        #region Verilmiş ədədlər siyahısını içindəki bütün ədədlər kvadratına yüksəlmiş array çevirən metod
+        //static void Main(string[] args)
+        //{
+        //    int[] b = { 12, 13, 14, 15, 16, 17, 18 };
+        //    ArrSquare(b);
+        //}
+        //static void ArrSquare(int[] a)
+        //{
+        //    for (int i = 0; i < a.Length; i++)
+        //    {
+        //        a[i] = a[i] * a[i];
+        //    }
+        //    for (int i = 0;a.Length > i; i++)
+        //    {
+        //        Console.WriteLine(a[i]);
+        //    }
+        //}
+        #endregion
+        #region Parametr olaraq 1 string dəyər qəbul edən və həmin string dəyəri icində bosluqlar qalmayacaq hala gətirən metod
+        //static void Main(string[] args)
+        //{
+        //    string a = "Hikmet Abbasov";
+        //    Console.WriteLine(Space(a));
+        //}
+        //static string Space(string s)
+        //{
+        //    return s.Replace(" ", "");
+        //}
+        #endregion
+        #region Parameter olaraq integer array variable-i ve bir integer value qebul eden ve hemin integer value-nu integer array-e yeni element kimi elave eden metod
+        //static void Main(string[] args)
+        //{
+        //    int[] array = { 1, 2, 3 };
+        //    int c = 10;
+        //    AddInt(array, c);
+        //}
+        //static void AddInt(int[] array, int b)
+        //{
+        //    int[] newArray= new int[array.Length+1];
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        newArray[i]= array[i];
+        //    }
+        //    newArray[newArray.Length-1] = b;
+        //    for (int i = 0;i < newArray.Length; i++)
+        //    {
+        //        Console.WriteLine(newArray[i]);
+        //    }
+        //}    
+        #endregion
+    }
+}
---
+++ Task 4/Optional Tasks/Program.cs
@@ -0,0 +1,67 @@
+﻿#region Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 3 == 0 && n % 7 == 0)
+    Console.WriteLine(n + " 3-e ve 7-e tam bolunur");
+else if (n % 3 == 0)
+    Console.WriteLine(n + " Yalniz 3-e tam bolunur");
+else if (n % 7 == 0)
+    Console.WriteLine(n + " Yalniz 7-e tam bolunur");
+else
+    Console.WriteLine("Eded hec birine tam bolunmur");
+#endregion
+#region Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin sayini tapin.
+//int n = Convert.ToInt32(Console.ReadLine());
+//int m = Convert.ToInt32(Console.ReadLine());
+//int count = 0;
+//while(n<=m)
+//{ 
+//    if (n % 2==1) 
+//        count++;  
+//    n++;
+//}
+//Console.WriteLine(count);
+#endregion
+#region Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+//int n = Convert.ToInt32(Console.ReadLine());
+//int m = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+//while (n <= m)
+//{
+//    if (n % 2 == 1)
+//        sum += n;
+//    n++;
+//}
+//Console.WriteLine(sum);
+#endregion
+#region Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin.
+//int n = Convert.ToInt32(Console.ReadLine());
+//int count = 0;
+//for (int i = 1; i <= n; i++)
+//{
+//    if (n % i == 0)
+//        count++; 
+//}
+//    if (count == 2)
+//    {
+//        Console.WriteLine("Eded sadedir");      
+//    }
+//    else if (count > 2)
+//    {
+//        Console.WriteLine("Eded murekkebdir");
+//    }
+//    else
+//        Console.WriteLine("Eded duzgun daxil edilmeyib");
+
+#endregion
+#region Verilmish arrayin icindeki cut ededlerin cemini tapin.
+//int[] array = { 29, 67, 28, 90, 12, 25, 61 };
+//int sum = 0;
+//for (int i = 0;i< array.Length; i++)
+//{
+//    if (array[i] % 2 == 0)
+//    {
+//        sum += array[i];
+//    }
+//}
+//Console.WriteLine(sum);
+#endregion
